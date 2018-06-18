c0460868016a14ddc34719e6ca8379064874d738
Alabhya Vaibhav <alabhya.vaibhav1997@gmail.com>
2018-06-18 14:13:11 +0530

    Dashboard Added, model made more robust

--- people-identifer/faceSimilarity.py
+++ people-identifer/faceSimilarity.py
@@ -4,6 +4,9 @@ Created on Mon May 28 14:45:58 2018
 
 @author: Alabhya Vaibhav
 """
+# python faceSimilarity.py --prototxt deploy.prototxt.txt --model res10_300x300_ssd_iter_140000.caffemodel
+
+
 import face_recognition
 import cv2
 import time
@@ -12,21 +15,41 @@ from firebase import firebase
 import datetime
 import string
 import random
-import glob, os
-#result = fb.patch(my_url + '/orders', {'id': 2})
+import numpy as np
+from imutils.video import VideoStream
+import argparse
+import imutils
+import glob
+
+
+
+##result = fb.patch(my_url + '/orders', {'id': 2})
 base = "https://inou-fde25.firebaseio.com/cam1/"
+u_base = "https://inou-fde25.firebaseio.com/"
 firebase = firebase.FirebaseApplication(base, None)
+def send_ds_cnt(ds_cnt):
+     result = firebase.patch(u_base + '/known_faces/',{'count': ds_cnt})
+     print(result)
     
 def sendData(user):
     ts = time.time()
     st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
-    node = ''.join(random.choices(string.ascii_uppercase + string.digits, k=3))
+    node = ''.join(random.choices(string.ascii_uppercase + string.digits, k=5))
     result = firebase.patch(base + str(user) + '/timestamp/' + node, {'time': st})
     print("[INFO] Data Posted to firebase")
-    print("[MSG]")
+    print("[Regular MSG]")
     print(result)
     
-
+def uniqueData():
+    ts = time.time()
+    st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+    getresult = firebase.get(u_base + '/unique_count/count/', None)
+    #print(getresult)
+    getresult += 1
+    postresult = firebase.patch(u_base + 'unique_count', {'time': st,'count':getresult})
+    print("[INFO] Data Posted to firebase")
+    print("[Unique MSG]")
+    print(postresult)
    
 #from threading import Thread
 
@@ -37,7 +60,7 @@ face_id = 0
 known_faces = [] 
 known_faces_encoding = []
 known_encodings = []
-def analysis(ds_cnt):
+def analysis(vs):
     global known_cnt
     global face_id
     
@@ -45,91 +68,131 @@ def analysis(ds_cnt):
     test_faces = []
     test_faces_encoding = [] 
 
-    cam = cv2.VideoCapture(0)
-    cam.set(3, 600) # set video width
-    cam.set(4, 600) # set video height
-    
-    face_detector = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-         
-    
         
     print ("[INFO] Charging up the camera :)")
-        
-#    try:    
-    ret, img = cam.read()
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    cv2.imshow("gray",gray)
-    faces = face_detector.detectMultiScale(gray, 1.3, 5) 
-    if(len(faces) > 0):
-        for (x,y,w,h) in faces:
-            cv2.rectangle(img, (x,y), (x+w,y+h), (255,0,0), 2)     
-            print ("[INFO] Taking your picture, Smile")
-                # Save the captured image into the datasets folder
-            cv2.imwrite("unknown-face/" + str(face_id) + ".jpg", gray[y:y+h,x:x+w])
-            cv2.imshow("img",img)
-            time.sleep(5)
-            face_id += 1   
-        
-        for test_face in os.listdir('C:/Users/Alabhya Vaibhav/Documents/Python Scripts/facerecog/unknown-face'):
-            test_faces.append(face_recognition.load_image_file("unknown-face/" + test_face))
-        print("[INFO] Test face loaded from datastore")
-        
-#        print(test_faces[2])
-#        exit()        
-        for test_face in test_faces:
-            print(test_face)
-            print(face_recognition.face_encodings(test_face))
-            print("==================================================")
-            test_faces_encoding.append(face_recognition.face_encodings(test_face)[0])  
-        print("[INFO] Test face encoding")
-        
-        for test in test_faces_encoding:
-            face_distances = face_recognition.face_distance(known_encodings,test)
-        print("[INFO] Distance finding done")
-        
-        for i, face_distance in enumerate(face_distances):
-            if(face_distance <= 0.4):
-                print("The test image has a distance of {:.2} from known image #{}".format(face_distance, i+1))
-                known_cnt += 1
-                sendData(i+1)
-                cam.release()
-                cv2.destroyAllWindows()
-                break
-            elif(face_distance > 0.41 and face_distance <= 0.60):
+#    distance = []
+    try:
+        _,frame = vs.read()
+        #frame = imutils.resize(frame, width=400)
+        (h, w) = frame.shape[:2]
+        blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)), 1.0,
+        (300, 300), (104.0, 177.0, 123.0))
+        net.setInput(blob)
+        detections = net.forward()
+        for i in range(0, detections.shape[2]):
+        # extract the confidence (i.e., probability) associated with the
+        # prediction
+            confidence = detections[0, 0, i, 2]
+    
+            # filter out weak detections by ensuring the `confidence` is
+            # greater than the minimum confidence
+            if confidence < args["confidence"]:
                 continue
-            elif(round(face_distance,2) > 0.61):
-                global moved_files_cnt
-                #print("let's move to the known files")
-                print(round(face_distance,2))
-                moved_files_cnt += 1
-                cam.release()
-                cv2.destroyAllWindows()
-                y_i=0
-                for test_face in os.listdir('C:/Users/Alabhya Vaibhav/Documents/Python Scripts/facerecog/unknown-face'):
-                     os.rename("C:/Users/Alabhya Vaibhav/Documents/Python Scripts/facerecog/unknown-face/" 
-                               + test_face,"C:/Users/Alabhya Vaibhav/Documents/Python Scripts/facerecog/known-face/" + str(ds_cnt + 1 + y_i) + ".jpg")
-                     y_i+=1
-                
-                test = 'C:/Users/Alabhya Vaibhav/Documents/Python Scripts/facerecog/unknown-face*'
-                r = glob.glob(test)
-                for i in r:
-                   os.remove(i)
-                
-                break
+    
+            # compute the (x, y)-coordinates of the bounding box for the
+            # object
+            box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+            (startX, startY, endX, endY) = box.astype("int")
             
-#    except Exception as e: print(e)
+            # draw the bounding box of the face along with the associated
+            # probability
+            #text = "{:.2f}%".format(confidence * 100)
+            #y = startY - 10 if startY - 10 > 10 else startY + 10
+            cv2.rectangle(frame, (startX, startY), (endX, endY),(0, 0, 255), 2)
+            #cv2.putText(frame, text, (startX, y),cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            cv2.imwrite("unknown-face/" + str(face_id) + ".jpg", gray[startY:endY,startX:endX])
+            face_id += 1
+#        ret, img = cam.read()
+#        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+#        faces = face_detector.detectMultiScale(gray, 1.3, 5) 
+#        if(len(faces) > 0):
+#            for (x,y,w,h) in faces:
+#                cv2.rectangle(img, (x,y), (x+w,y+h), (255,0,0), 2)     
+#                print ("[INFO] Taking your picture, Smile")
+#                    # Save the captured image into the datasets folder
+#                cv2.imwrite("unknown-face/" + str(face_id) + ".jpg", gray[y:y+h,x:x+w])
+#                #cv2.imshow("img",gray)
+        try:
+            faces_name_unknown=os.listdir('C:/Users/Alabhya Vaibhav/Documents/Python Scripts/facerecog/unknown-face')
+            for test_face in os.listdir('C:/Users/Alabhya Vaibhav/Documents/Python Scripts/facerecog/unknown-face'):
+                test_faces.append(face_recognition.load_image_file("unknown-face/" + test_face))
+            
+            print("[INFO] Test face loaded from datastore")      
+            for test_face in test_faces:
+                test_faces_encoding.append(face_recognition.face_encodings(test_face)[0])  
+            print("[INFO] Test face encoding")
+            
+            for test in test_faces_encoding:
+                face_distances = face_recognition.face_distance(known_encodings,test)
+                distance = np.array(face_distances).tolist()
+                min_val = min(distance)
+                i = distance.index(min_val)
+                print(i,min_val)
+                a = test_faces_encoding.index(test)
+                b = faces_name_unknown[a]
+                   
+                if(min(face_distances) <= 0.5):
+                    print("The test image has a distance of {:.2} from known image #{}".format(face_distances[i], i+1))
+                    known_cnt += 1
+                    print ("Known image is ",str(b))
+                    os.rename("C:/Users/Alabhya Vaibhav/Documents/Python Scripts/facerecog/unknown-face/" + str(b) ,"C:/Users/Alabhya Vaibhav/Documents/Python Scripts/facerecog/bin/" + str(time.time()) + ".jpg")
+                    sendData(i+1)
+                else:
+                    global moved_files_cnt
+                    #print(round(face_distances[],2))
+                    #moved_files_cnt += 1
+                    uniqueData()
+                    print("Need to move file",str(b))
+                    os.rename("C:/Users/Alabhya Vaibhav/Documents/Python Scripts/facerecog/unknown-face/"+ str(b) ,"C:/Users/Alabhya Vaibhav/Documents/Python Scripts/facerecog/known-face/" + str(ds_cnt + 1) + ".jpg")
+    
+            #print(type(face_distances))
+                #distance.append(face_distances)
+    
+            print("[INFO] Distance finding done")
+            
+        #print(type(distance))
         
-    print("unique visitors" + str(moved_files_cnt))
-    print()
-    print("Total new people" + str(ds_cnt))
+        #i = face_distances.index(min(face_distances))
+        
+        except Exception as e:
+            print("Inner exception ,file loading ",str(e))
+            if(str(e) == "list index out of range" or str(e) in "cannot identify image file 'unknown-face/'"):
+                files = glob.glob('C:/Users/Alabhya Vaibhav/Documents/Python Scripts/facerecog/unknown-face/*')
+                for f in files:
+                    os.remove(f)
+            
+                                
+    except Exception as e:
+        print("outer exception",str(e))
+        
+#    print("unique visitors" + str(moved_files_cnt))
+#    print()
+#    print("Total people" + str(ds_cnt))
     
     
     #print("Total re-visits" + str(known_cnt))
-
+ap = argparse.ArgumentParser()
+ap.add_argument("-p", "--prototxt", required=True,help="path to Caffe 'deploy' prototxt file")
+ap.add_argument("-m", "--model", required=True,help="path to Caffe pre-trained model")
+ap.add_argument("-c", "--confidence", type=float, default=0.8,help="minimum probability to filter weak detections")
+args = vars(ap.parse_args())
+net = cv2.dnn.readNetFromCaffe(args["prototxt"], args["model"])
+vs = cv2.VideoCapture(0)
+#vs.set(3,1280)
+#vs.set(4,1024)
 while(True):
+       
+    #cam = cv2.VideoCapture(0)
+    #cam.set(3, 600) # set video width
+    #cam.set(4, 600) # set video height
     ds_cnt = 0
+    known_faces = [] 
+    known_faces_encoding = []
+    known_encodings = []
     for file in os.listdir('C:/Users/Alabhya Vaibhav/Documents/Python Scripts/facerecog/known-face'):
         ds_cnt += 1
+    send_ds_cnt(ds_cnt)
     try:
         for file in os.listdir('C:/Users/Alabhya Vaibhav/Documents/Python Scripts/facerecog/known-face'):
             known_faces.append(face_recognition.load_image_file("known-face/" + file))
@@ -141,12 +204,8 @@ while(True):
             known_encodings.append(x)
     except:
         print("Encoding error, Retrying")
-    print(len(known_encodings))
-    analysis(ds_cnt)
-    cv2.destroyAllWindows()
-    time.sleep(15)
-	
-cam.release()
-
+    #print(len(known_encodings))
+    time.sleep(1)
+    analysis(vs)
 
 
